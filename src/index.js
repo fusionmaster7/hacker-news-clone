4877b6acdcf985913f0b6575b5933415e1b68809
Hardik Singh <hardiksingh297@gmail.com>
2020-06-03 16:53:08 +0530

    Added CRUD operations

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const typeDefs = `type Query {
-    info : String!
-}`;
+let links = [
+  {
+    id: "link-0",
+    url: "https://google.com",
+    description: "The most popular search engine",
+  },
+];
+
+let idCount = links.length;
 
 const resolvers = {
   Query: {
-    info: () => null,
+    info: () => `Hacker News clone API`,
+    feed: () => links,
+    link: (parent, args) => {
+      const id = args.id;
+      return links[id[id.length - 1]];
+    },
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        url: args.url,
+        description: args.description,
+      };
+      links.push(link);
+      return link;
+    },
+    delete: (parent, args) => {
+      let newLinks = links.filter((e) => e.id !== args.id);
+      const deletedLink = links.find((e) => e.id == args.id);
+      links = newLinks;
+      return deletedLink;
+    },
+    update: (parent, args) => {
+      const index = links.findIndex((e) => e.id == args.id);
+      const updatedLink = {
+        id: args.id,
+        url: args.url || links[index].url,
+        description: args.description || links[index].description,
+      };
+      links[index] = updatedLink;
+      return updatedLink;
+    },
   },
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers,
 });
 
